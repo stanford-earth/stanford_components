5344e16a735ec6bbe9a585ab5af8e937ba6d30a4
Jesse James Arnold <jesse.james@exygy.com>
2017-07-17 14:50:19 -0700

    EARTH-282: Setting up multi column masonry cards (#50)
    * EARTH-282: Setting up multi column masonry cards
    * EARTH-282: Add if else to make is-column-width the default class

--- patterns/molecules/masonry-blocks/js/masonry-blocks.js
+++ patterns/molecules/masonry-blocks/js/masonry-blocks.js
@@ -4,7 +4,7 @@
   $(window).load(function () {
     $('.js-masonry').masonry({
       // set itemSelector so .grid-sizer is not used in layout
-      itemSelector: '.js-masonry .js-masonry-item',
+      columnWidth: '.is-column-width',
       percentPosition: true
     })
 
